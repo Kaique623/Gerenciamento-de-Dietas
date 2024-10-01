88d6d84cf872efd03b50323d960a4b3f12ac79ff
Kaique62 <kaiquegm623@gmail.com>
2024-10-01 17:16:35 -0300

    coisas

--- main.Designer.cs
+++ main.Designer.cs
@@ -28,19 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.MainSplitScreen = new System.Windows.Forms.SplitContainer();
+            this.RecarregarPagina = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SearchText = new System.Windows.Forms.TextBox();
+            this.AlimentosButton = new System.Windows.Forms.Button();
+            this.DietaTabButton = new System.Windows.Forms.Button();
+            this.UsuariosTabButton = new System.Windows.Forms.Button();
+            this.UserDataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.MainSplitScreen)).BeginInit();
+            this.MainSplitScreen.Panel1.SuspendLayout();
+            this.MainSplitScreen.Panel2.SuspendLayout();
+            this.MainSplitScreen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UserDataGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainSplitScreen
+            // 
+            this.MainSplitScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainSplitScreen.IsSplitterFixed = true;
+            this.MainSplitScreen.Location = new System.Drawing.Point(0, 0);
+            this.MainSplitScreen.Name = "MainSplitScreen";
+            this.MainSplitScreen.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // MainSplitScreen.Panel1
+            // 
+            this.MainSplitScreen.Panel1.BackColor = System.Drawing.Color.LightGreen;
+            this.MainSplitScreen.Panel1.Controls.Add(this.RecarregarPagina);
+            this.MainSplitScreen.Panel1.Controls.Add(this.EditButton);
+            this.MainSplitScreen.Panel1.Controls.Add(this.addButton);
+            this.MainSplitScreen.Panel1.Controls.Add(this.searchButton);
+            this.MainSplitScreen.Panel1.Controls.Add(this.SearchText);
+            this.MainSplitScreen.Panel1.Controls.Add(this.AlimentosButton);
+            this.MainSplitScreen.Panel1.Controls.Add(this.DietaTabButton);
+            this.MainSplitScreen.Panel1.Controls.Add(this.UsuariosTabButton);
+            // 
+            // MainSplitScreen.Panel2
+            // 
+            this.MainSplitScreen.Panel2.BackColor = System.Drawing.Color.White;
+            this.MainSplitScreen.Panel2.Controls.Add(this.UserDataGrid);
+            this.MainSplitScreen.Size = new System.Drawing.Size(1148, 684);
+            this.MainSplitScreen.SplitterDistance = 110;
+            this.MainSplitScreen.SplitterWidth = 1;
+            this.MainSplitScreen.TabIndex = 0;
+            // 
+            // RecarregarPagina
+            // 
+            this.RecarregarPagina.BackColor = System.Drawing.Color.White;
+            this.RecarregarPagina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.RecarregarPagina.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RecarregarPagina.Location = new System.Drawing.Point(360, 53);
+            this.RecarregarPagina.Name = "RecarregarPagina";
+            this.RecarregarPagina.Size = new System.Drawing.Size(142, 29);
+            this.RecarregarPagina.TabIndex = 7;
+            this.RecarregarPagina.Text = "Recarregar Dados";
+            this.RecarregarPagina.UseVisualStyleBackColor = false;
+            // 
+            // EditButton
+            // 
+            this.EditButton.BackColor = System.Drawing.Color.White;
+            this.EditButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.EditButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EditButton.Location = new System.Drawing.Point(193, 53);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(142, 29);
+            this.EditButton.TabIndex = 6;
+            this.EditButton.Text = "Editar";
+            this.EditButton.UseVisualStyleBackColor = false;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.White;
+            this.addButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addButton.Location = new System.Drawing.Point(23, 53);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(142, 29);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Adicionar";
+            this.addButton.UseVisualStyleBackColor = false;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(469, 17);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(33, 31);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "🔎";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // SearchText
+            // 
+            this.SearchText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SearchText.Location = new System.Drawing.Point(23, 18);
+            this.SearchText.Name = "SearchText";
+            this.SearchText.Size = new System.Drawing.Size(446, 29);
+            this.SearchText.TabIndex = 3;
+            // 
+            // AlimentosButton
+            // 
+            this.AlimentosButton.BackColor = System.Drawing.Color.White;
+            this.AlimentosButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AlimentosButton.Location = new System.Drawing.Point(1048, 18);
+            this.AlimentosButton.Name = "AlimentosButton";
+            this.AlimentosButton.Size = new System.Drawing.Size(78, 64);
+            this.AlimentosButton.TabIndex = 2;
+            this.AlimentosButton.Text = "Alimentos";
+            this.AlimentosButton.UseVisualStyleBackColor = false;
+            this.AlimentosButton.Click += new System.EventHandler(this.TabButtonClick);
+            // 
+            // DietaTabButton
+            // 
+            this.DietaTabButton.BackColor = System.Drawing.Color.White;
+            this.DietaTabButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DietaTabButton.Location = new System.Drawing.Point(964, 18);
+            this.DietaTabButton.Name = "DietaTabButton";
+            this.DietaTabButton.Size = new System.Drawing.Size(78, 64);
+            this.DietaTabButton.TabIndex = 1;
+            this.DietaTabButton.Text = "Dietas";
+            this.DietaTabButton.UseVisualStyleBackColor = false;
+            this.DietaTabButton.Click += new System.EventHandler(this.TabButtonClick);
+            // 
+            // UsuariosTabButton
+            // 
+            this.UsuariosTabButton.BackColor = System.Drawing.Color.White;
+            this.UsuariosTabButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UsuariosTabButton.Location = new System.Drawing.Point(880, 18);
+            this.UsuariosTabButton.Name = "UsuariosTabButton";
+            this.UsuariosTabButton.Size = new System.Drawing.Size(78, 64);
+            this.UsuariosTabButton.TabIndex = 0;
+            this.UsuariosTabButton.Text = "Usuários";
+            this.UsuariosTabButton.UseVisualStyleBackColor = false;
+            this.UsuariosTabButton.Click += new System.EventHandler(this.TabButtonClick);
+            // 
+            // UserDataGrid
+            // 
+            this.UserDataGrid.AllowUserToAddRows = false;
+            this.UserDataGrid.AllowUserToDeleteRows = false;
+            this.UserDataGrid.AllowUserToResizeColumns = false;
+            this.UserDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.UserDataGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.UserDataGrid.BackgroundColor = System.Drawing.Color.White;
+            this.UserDataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedVertical;
+            this.UserDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.UserDataGrid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.UserDataGrid.Location = new System.Drawing.Point(3, 3);
+            this.UserDataGrid.Name = "UserDataGrid";
+            this.UserDataGrid.ReadOnly = true;
+            this.UserDataGrid.RowTemplate.Height = 25;
+            this.UserDataGrid.Size = new System.Drawing.Size(1142, 558);
+            this.UserDataGrid.TabIndex = 0;
             // 
             // MainApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1148, 684);
+            this.Controls.Add(this.MainSplitScreen);
             this.Name = "MainApp";
             this.Text = "Gerenciamento de Dietas";
+            this.MainSplitScreen.Panel1.ResumeLayout(false);
+            this.MainSplitScreen.Panel1.PerformLayout();
+            this.MainSplitScreen.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainSplitScreen)).EndInit();
+            this.MainSplitScreen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.UserDataGrid)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private SplitContainer MainSplitScreen;
+        private Button DietaTabButton;
+        private Button UsuariosTabButton;
+        private DataGridView UserDataGrid;
+        private Button AlimentosButton;
+        private Button RecarregarPagina;
+        private Button EditButton;
+        private Button addButton;
+        private Button searchButton;
+        private TextBox SearchText;
     }
 }
--- main.cs
+++ main.cs
@@ -1,10 +1,77 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace Gerenciamento_de_Dietas
 {
     public partial class MainApp : Form
     {
+        public static MySqlConnection connection = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=dietas;User=root;Password=acesso123;");
+        
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+        List<string> tables = new List<string>() {"usuario", "dieta", "alimento","refeicao", "refeicao_alimento"};
+
+        string current_table = "usuario";
+
+        DataSet data = new DataSet();
+
         public MainApp()
         {
             InitializeComponent();
+            connection.Open();
+
+            LoadAllTables("usuario");
+        }
+
+        void LoadAllTables(string table)
+        {
+            data.Tables.Clear();
+            foreach (string value in tables)
+            {
+                data.Tables.Add();
+                adapter.SelectCommand = new MySqlCommand($"Select * FROM {value}", connection);
+                adapter.Fill(data.Tables[tables.IndexOf(value)]);
+            }
+            UserDataGrid.DataSource = data.Tables[tables.IndexOf(table)];
+        }
+
+        private void TabButtonClick(object sender, EventArgs e)
+        {
+            string buttonText = ((Button)sender).Text;
+
+            if (buttonText == "Usuários")
+            {
+                current_table = "usuario";
+                UserDataGrid.DataSource = data.Tables[0];
+            } 
+            else if (buttonText == "Dietas")
+            {
+                current_table = "dieta";
+                UserDataGrid.DataSource = data.Tables[1];
+            }    
+            else if(buttonText == "Alimentos")
+            {
+                current_table = "alimento";
+                UserDataGrid.DataSource = data.Tables[2];
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string columnsConditional = "WHERE ";
+            foreach (DataGridViewColumn column in UserDataGrid.Columns)
+            {
+                if (columnsConditional == "WHERE ")
+                    columnsConditional += $"{column.Name} LIKE '%{SearchText.Text}%'";
+                else
+                    columnsConditional += $" OR {column.Name} LIKE '%{SearchText.Text}%'";
+            }
+
+            DataSet ds = new DataSet();
+            ds.Tables.Add();
+            adapter.SelectCommand = new MySqlCommand($"SELECT * FROM {current_table} {columnsConditional}" , connection);
+            adapter.Fill(ds.Tables[0]);
+            UserDataGrid.DataSource = ds.Tables[0];
         }
     }
 }
